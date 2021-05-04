eb5aa5a817ccc1d4a6895ec2bfec6c0d1fad5f15
Leonardo Moreno <jmoreno3102@gmail.com>
2021-05-04 18:22:10 -0400

    Add log file && try statements

--- quad_coin_enigma_api.py
+++ quad_coin_enigma_api.py
@@ -9,14 +9,8 @@ import requests
 import subprocess
 import json
 import datetime
-import socket
-import ssl
 
-# for encoding/decoding messages in base64
-from base64 import urlsafe_b64decode, urlsafe_b64encode
-
-# set socket timeout to 10 secs
-socket.setdefaulttimeout(8)
+from json.decoder import JSONDecodeError
 
 def get_last_transactions(key):
 
@@ -24,8 +18,16 @@ def get_last_transactions(key):
                             f"https://block.io/api/v2/get_transactions/?api_key={key}&type=sent"
                             ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     output = proc.stdout.read()
-    #print(output)
-    d = json.loads(output)
+
+    try:
+        d = json.loads(output)
+    except JSONDecodeError as e:
+        print("DECODING JSON HAS FAILED")
+        print(output)
+        print("DECODING JSON HAS FAILED")
+
+        return 0.0, "NA"
+
     if d["data"]["network"]:
         crypto = d["data"]["network"]
 
@@ -79,41 +81,51 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # initalize ids and auth token
-    ids = {}
+    ids = []
     batch = 0.0
     enigma_auth = get_enigma_auth(args.username, args.password)
     print(enigma_auth)
 
     while True:
 
-        try:
+        with open("output.log", "a+") as out:
+
             # get current time
             now = datetime.datetime.now()
 
             # every day at 2am a new auth token will be generated
             if now.hour == 2 and now.minute == 1:
                 enigma_auth = get_enigma_auth(args.username, args.password)
-                last_hash = ids[last_id]
-                ids = {}
-                ids[last_id] = last_hash
+                last_txid = ids[-1]
+                ids = []
+                ids.append(last_txid)
                 print(f'New auth token: {enigma_auth}')
                 print("Cleared IDs")
 
             to_buy_back, txid = get_last_transactions(args.api_key)
 
+            if txid == "NA":
+                time.sleep(60)
+                continue
+
             if txid not in ids:
-                ids[txid] = txid
-                last_id = txid
+
+                ids.append(txid)
                 print(ids)
 
                 if to_buy_back < 0.001 and batch < 0.001:
                     batch += to_buy_back
                     print(f'Current buy back (batches) - {batch}')
+                    print(f'Current buy back (batches) - {batch}', file=out)
+                    out.close()
 
                 else:
                     sum_to_buy = batch + to_buy_back
                     print("="*50)
-                    print(f'BUYING THIS MUCH FROM ENIGMA - {sum_to_buy}')
+                    print("-"*50)
+                    print(f'BUYING THIS MUCH FROM ENIGMA - {sum_to_buy} - {txid}')
+                    print(f'BUYING THIS MUCH FROM ENIGMA - {sum_to_buy} - {txid} - {now}', file=out)
+                    print("-"*50)
                     proc = subprocess.Popen(["curl", "--location",
                                             "--request", "POST",
                                             "https://api.enigma-securities.io/trade",
@@ -128,11 +140,8 @@ if __name__ == "__main__":
                     print("="*50)
                     batch = 0.0
 
-        except socket.timeout:
-            print("SOCKET TIMEOUT TRYING AGAIN")
-        except ssl.SSLError as err:
-            print("SSL ERROR TRYING AGAIN")
+                    out.close()
 
-        ## check every 30 secs
-        time.sleep(30)
+            ## check every 60 secs
+            time.sleep(60)
 
